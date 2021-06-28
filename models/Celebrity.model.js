338d40a8b9d0cd2c6f175bcd257e0d92b6aeba8e
Israel Zambrana <zambrana90@gmail.com>
2021-06-28 12:11:07 +0200

    till users

---
+++ models/Celebrity.model.js
@@ -0,0 +1,27 @@
+//  Add your code here
+
+const mongoose = require("mongoose");
+const celebritySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    occupation: {
+      type: String,
+      required: true,
+    },
+    catchPhrase: {
+      type: String,
+      required: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
---
+++ routes/movies.routes.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
+
+module.exports.home = (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      res.render("movies/movies.hbs", { movies });
+    })
+    .catch((e) => console.error(e));
+};
+
+module.exports.createMovie = (req, res, next) => {
+  Celebrity.find().then((celebrities) => {
+    res.render("movies/new-movie.hbs", { celebrities });
+  });
+};
+
+module.exports.doCreateMovie = (req, res, next) => {
+  Movie.create(req.body)
+    .then((movie) => {
+      res.redirect("/movies");
+    })
+    .catch((e) => res.render("movies/new-movie.hbs"));
+};
+
+module.exports.idMovie = (req, res, next) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .populate("cast")
+    .then((movie) => {
+      res.render("movies/movie-details.hbs", movie);
+    })
+    .catch((e) => console.error(e));
+};
+
+module.exports.editMovie = (req, res, next) => {
+  Promise.all([Movie.findById(req.params.id), Celebrity.find()])
+    .then(([movie, celebrities]) => {
+      res.render(`movies/edit-movie.hbs`, { movie, celebrities });
+    })
+    .catch((e) => console.error(e));
+};
+
+module.exports.doEditMovie = (req, res, next) => {
+  Movie.findByIdAndUpdate(req.params.id, req.body)
+    .then((movie) => res.redirect(`/movies`))
+    .catch((e) => console.error(e));
+};
+
+module.exports.deleteMovie = (req, res, next) => {
+  const { id } = req.params;
+  Movie.findByIdAndDelete(id)
+    .then((movie) => res.redirect(`/movies`))
+    .catch((e) => console.error(e));
+};
